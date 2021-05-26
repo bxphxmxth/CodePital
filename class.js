5ab305beff992989b4c2f00a46f2c4e0573efd88
bxphxmxth <fushs@hotmail.fr>
2021-05-26 10:43:30 +0200

    marc

--- class.js
+++ class.js
@@ -1,3 +1,21 @@
 class Patients{
-     constructor(nom,maladie,argent,)
+     constructor(nom,maladie,argent,poche,etat){
+          this.nom = nom;
+          this.maladie = maladie;
+          this.argent = argent;
+          this.poche = poche;
+          this.etat = etat;
+          this.seDeplacer = (depart,arrivee) =>{
+
+          };
+          this.prendreMedoc = (medicament, poche) =>{
+
+          };
+          this.payer = (vendeur,objet) =>{
+
+          };
+     }
 }
+
+
+export {Patients};
---
+++ objets.js
@@ -0,0 +1,43 @@
+let docteur = {
+     nom : "Riviera",
+     argent: 50,
+     cabinet: [],
+     salleAttente: [],
+     patientIn(){
+
+     },
+     patientOut(){
+
+     },
+     prixConsultation(){
+
+     },
+
+}
+
+let chat = {
+     nom: "Edgar",
+     miauler(){
+          //timemachin
+     },
+}
+
+export {docteur,chat,cimetiere}
+
+let pharmacie = {
+     nom: "pharmacie",
+     caisse: 20,
+     assezArgent(){
+          
+     },
+     pasDargent(){
+          cimetiere.push();
+          Patients.etat == "mort";
+     },
+}
+
+
+let cimetiere = {
+     nom: "cimetière",
+     contenu: [],
+}
